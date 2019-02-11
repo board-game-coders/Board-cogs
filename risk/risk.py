5f0c6f2c6b351c2adc416cee8289b1710c73e13e
NeuroAssassin <wrbrown70@yahoo.com>
2019-02-11 14:43:52 -0500

    Add some more starter stuff

--- risk/risk.py
+++ risk/risk.py
@@ -1,7 +1,22 @@
 import discord
-from redbot.core import commands
+from redbot.core import commands, Config
+from redbot.core.data_manager import bundled_data_path
+from redbot.core.data_manager import cog_data_path
 from random import randint
 
 class Risk(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.runningin = []
+        self.config = Config.get_conf(self, identifier=5849752632)
+        self.config.register_guild(
+
+        )
+
+    @commands.guild_only()
+    @commands.command()
+    async def risk(self, ctx):
+        """Plays a game of Risk.  In heavy development, discouraged from use"""
+        if ctx.channel.id in self.runningin:
+            return await ctx.send('There is already a game running in this channel.')
+        self.runningin.append(ctx.channel.id)
